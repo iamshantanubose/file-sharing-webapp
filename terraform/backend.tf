96deeac9e5a4b5aaf7be259e139d80a33a5072a8
Shantanu Bose <iamshantanubose@gmail.com>
2025-01-18 22:39:07 +0530

    Changes in backend bucket name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-storage"
+    bucket         = "terraform-state-storage-shaan"
     key            = "file-sharing-webapp/terraform.tfstate"
     region         = "us-east-1"
   }

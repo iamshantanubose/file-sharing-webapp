b353f1a8a9f46180ad85e6fbb059e62cf0c4c5b1
Shantanu Bose <iamshantanubose@gmail.com>
2025-01-19 01:13:07 +0530

    Correction for Mistake in Paste

--- main.tf
+++ main.tf
@@ -143,4 +143,20 @@ resource "aws_security_group" "file_sharing_sg" {
 
 # Dynamic Amazon Linux AMI Lookup
 data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+output "s3_website_url" {
+  value = aws_s3_bucket.frontend_bucket.website_endpoint
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.file_sharing_instance.public_ip
+}
  

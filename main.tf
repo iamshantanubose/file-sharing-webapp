344327613b9a31e90b77540b14e3f349202aa439
Shantanu Bose <iamshantanubose@gmail.com>
2025-01-19 00:28:07 +0530

    More changes

--- main.tf
+++ main.tf
@@ -71,6 +71,17 @@ resource "aws_security_group" "file_sharing_sg" {
   }
 }
 
+# Declare the data source for the Amazon Linux AMI
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
 resource "aws_instance" "file_sharing_instance" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"

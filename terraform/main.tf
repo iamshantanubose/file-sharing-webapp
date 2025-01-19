bd5a2205311941cb34ba1c50f61feaec75807933
Shantanu Bose <iamshantanubose@gmail.com>
2025-01-19 14:14:12 +0530

    Correction-3

--- terraform/main.tf
+++ terraform/main.tf
@@ -141,12 +141,3 @@ data "aws_ami" "amazon_linux" {
     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 }
-
-# Outputs
-output "s3_website_url" {
-  value = aws_s3_bucket_website_configuration.frontend_website.website_endpoint
-}
-
-output "signaling_server_ip" {
-  value = aws_instance.signaling_server.public_ip
-}
